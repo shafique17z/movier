070745e1174fa79f08544da7862f0899658a12da
Shafique Ahmed <shafiqued17z@gmail.com>
2023-06-25 19:27:24 +0500

    Movie screen halfway there

--- components/trendingMovies.js
+++ components/trendingMovies.js
@@ -7,9 +7,16 @@ import {
 } from 'react-native'
 import React from 'react'
 import Carousel from 'react-native-snap-carousel'
+import { useNavigation } from '@react-navigation/native'
 
 const { width, height } = Dimensions.get('window')
+
 export default function TrendingMovies({ data }) {
+  const navigation = useNavigation()
+  const handleClick = (item) => {
+    navigation.navigate('Movie', item)
+  }
+
   return (
     <View className='mb-8'>
       <Text className='text-white text-xl mx-4 mb-4'>Trending</Text>
@@ -17,7 +24,9 @@ export default function TrendingMovies({ data }) {
       {/* Carousel configuration */}
       <Carousel
         data={data}
-        renderItem={({ item }) => <MovieCard item={item} />}
+        renderItem={({ item }) => (
+          <MovieCard item={item} handleClick={handleClick} />
+        )}
         firstItem={1}
         inactiveSlideOpacity={0.6}
         slideStyle={{ display: 'flex', alignItems: 'center' }}
@@ -28,9 +37,9 @@ export default function TrendingMovies({ data }) {
   )
 }
 
-const MovieCard = ({ item }) => {
+const MovieCard = ({ item, handleClick }) => {
   return (
-    <TouchableWithoutFeedback>
+    <TouchableWithoutFeedback onPress={() => handleClick(item)}>
       <Image
         source={require('../assets/poster2.jpg')}
         style={{ width: width * 0.6, height: height * 0.4 }}
--- screens/MovieScreen.js
+++ screens/MovieScreen.js
@@ -1,10 +1,82 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  Image,
+  Dimensions,
+  TouchableOpacity,
+  ScrollView,
+  Platform,
+} from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { useNavigation, useRoute } from '@react-navigation/native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { ChevronLeftIcon } from 'react-native-heroicons/outline'
+import { HeartIcon } from 'react-native-heroicons/solid'
+import { styles, theme } from '../theme'
+import { LinearGradient as Gradient } from 'expo-linear-gradient'
+
+const ios = Platform.OS == 'ios'
+const topMargin = ios ? '' : ' mt-3'
+var { width, height } = Dimensions.get('window')
 
 export default function MovieScreen() {
+  const { params: item } = useRoute()
+  const navigation = useNavigation()
+  const { isFavorite, toggleFavorite } = useState(false)
+
+  useEffect(() => {
+    //call api to get movie details
+  }, [item])
+
   return (
-    <View>
-      <Text>MovieScreen</Text>
-    </View>
+    <ScrollView
+      contentContainerStyle={{ paddingBottom: 1000 }}
+      className='flex-1 bg-neutral-900'
+    >
+      {/* back button and movie poster */}
+
+      <View className='w-full'>
+        <SafeAreaView
+          className={
+            'absolute z-20 w-full flex-row justify-between items-center px-4 ' +
+            topMargin
+          }
+        >
+          <TouchableOpacity
+            style={styles.background}
+            className='rounded-xl p-1'
+          >
+            <ChevronLeftIcon size='28' strokeWidth={2.5} color='white' />
+          </TouchableOpacity>
+
+          <TouchableOpacity onPress={() => toggleFavorite(!isFavorite)}>
+            <HeartIcon
+              size='35'
+              color={isFavorite ? theme.background : 'white'}
+            />
+          </TouchableOpacity>
+        </SafeAreaView>
+        <View>
+          <Image
+            source={require('../assets/poster2.jpg')}
+            style={{
+              width,
+              height: height * 0.55,
+            }}
+          />
+          <Gradient
+            colors={['transparent', 'rgba(23,23,23,0.8)', 'rgba(23,23,23,1)']}
+            style={{
+              width,
+              height: height * 0.55,
+              position: 'absolute',
+              bottom: 0,
+            }}
+            start={{ x: 0.5, y: 0 }}
+            end={{ x: 0.5, y: 1 }}
+          />
+        </View>
+      </View>
+    </ScrollView>
   )
 }
